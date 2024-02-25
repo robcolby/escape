d6e281f7f91655992ce8eab81db476792925b38b
Rob Colby <robcolby2@gmail.com>
2024-02-25 01:29:39 -0800

    player movement and house_map

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,28 +1,33 @@
 extends CharacterBody2D
 
+@export var speed = 200 # how fast the player will move (pixels / second )
+var screen_size # size of the game window
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+func _ready():
+	screen_size = get_viewport_rect().size
+	
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var velocity = Vector2.ZERO # The players movement vector
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+#	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x > 0:
+		$AnimatedSprite2D.animation = "right"
+	elif velocity.x < 0:
+		$AnimatedSprite2D.animation = "left"
 
-	move_and_slide()
