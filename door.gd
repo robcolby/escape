b17a5848151fe41d89fb47d07c8d5cf36f046d59
Rob Colby <robcolby2@gmail.com>
2024-03-11 00:20:27 -0700

    door finished, working on bed

--- door.gd
+++ door.gd
@@ -6,6 +6,7 @@ var at_door = false
 
 func _ready():
 	$CollisionShape2D.disabled = false # Door starts as closed
+	$door_Timer.stop() # Ensure timer is off
 
 func _on_door_body_entered(body):
 	if body.name == "Player":
@@ -17,18 +18,18 @@ func _on_door_body_exited(body):
 		at_door = false
 		print("Player cannot trigger the door, ", door_id)
 
+func _on_door_timer_timeout():
+	$door_Timer.stop()
+	
 func get_input():
-	if Input.is_action_pressed("trigger_action") and at_door == true:
+	if Input.is_action_pressed("trigger_action") and at_door == true and $door_Timer.is_stopped():
 		is_open = !is_open
 		$CollisionShape2D.disabled = is_open # Disable collision if door is open
 		if $AnimatedSprite2D.frame == 0:
 			$AnimatedSprite2D.set_frame (1)
 		else:
 			$AnimatedSprite2D.set_frame (0)
+		$door_Timer.start()
 
 func _process(delta): 
 	get_input()
-
-
-
-
